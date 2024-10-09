12b12924505d66507300415979784b5b7fc4d78c
chilldeleuze <16662248+chilldeleuze@users.noreply.github.com>
2024-10-09 17:10:09 +0200

    add simple calculation logic

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let operator = '';
 let num1 = '';
 let num2 = '';
 let input = []
-let operators = ["*/+-"]
+let result = '';
+const operators = ['+', '-', '/', 'x'];
 const display = document.querySelector("div.calculator__display");
 const calculator = document.querySelector("div.calculator__buttons");
 
@@ -13,10 +14,32 @@ const calculator = document.querySelector("div.calculator__buttons");
 // but it also gives us 
 calculator.addEventListener("click", (e) => {
     let target = e.target;
-    if (target.matches("div.calculator__button") && operator === '') {
+    let charInput = target.textContent;
+
+    if(!target.matches("div.calculator__button")) {
+        return
+    }
+
+    if (charInput === 'CE') {
+        cleanVariables();
+        return;
+    }
+
+    if (operator === '' && !operators.includes(target.textContent)) {
         num1 += target.textContent
         console.log("num1: " + num1)
-    } 
+        return
+    } else if (operators.includes(target.textContent) && num1 !== '') {
+        operator = target.textContent
+        console.log("operator: " + operator)
+        return
+    } else if (operator !== '' && target.textContent !== "=") {
+        num2 += target.textContent
+        console.log("num2: " + num2)
+        return
+    } else if (operator !== '' && num1 !== '' && num2 !== '' && target.textContent === "=") {
+        console.log(operate(operator, num1, num2))
+    }
     
     })
 
@@ -57,13 +80,17 @@ function divide(num1, num2) {
     return num1 / num2    
 }
 
+
+
 function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2)
     switch (operator) {
         case '+':
             return add(num1, num2);
         case '-':
             return subtract(num1, num2);
-        case '*':
+        case 'x':
             return multiply(num1, num2);
         case '/':
             return divide(num1, num2);
